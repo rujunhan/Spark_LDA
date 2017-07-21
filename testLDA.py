5bc744e230a876b2d02030f682e94e096b5fbf09
rhan1207 <hanrj1207@yahoo.com>
2017-07-21 19:58:53 -0400

    vocabulary built

--- testLDA.py
+++ testLDA.py
@@ -1,18 +1,21 @@
-#### run this line in terminal: spark-submit --packages com.databricks:spark-xml_2.10:0.4.1 testLDA.py !!! 
+#### run this line in terminal: spark-submit --packages com.databricks:spark-xml_0.10:0.4.1 testLDA.py !!! 
 
 from pyspark import SparkContext, SparkConf
 from pyspark.mllib.clustering import LDA, LDAModel
 from pyspark.mllib.linalg import Vectors
 from pyspark.sql import SQLContext
-
+import copy
 import numpy as np
 import nltk
+nltk.data.path.append("/home/rjh347/nltk_data/")
+from nltk.corpus import stopwords
+from nltk import word_tokenize
+from nltk import ngrams
 import string
 import re
 from nltk.corpus import stopwords
 from nltk import word_tokenize
 from nltk import ngrams
-
 import os
 import subprocess
 
@@ -22,69 +25,53 @@ conf = SparkConf().setAppName(App_Name)
 sc = SparkContext(conf=conf)
 sqlContext = SQLContext(sc)
 
+
 #obtain all files in a diretory
-def get_filenames(path):
-	cmd = ['hdfs', 'dfs', '-ls', path]
-	files = [f.split(' ')[-1] for f in subprocess.check_output(cmd).strip().split('\n')]
-        return files[1:]
 
-#hedline = sqlContext.read.format("com.databricks.spark.xml").option("rowTag", "hedline").load("lda/temp/1815836.xml")
+def parse_xml(x):
 
-#print hedline.take(1)[0][0]
-
-# get xml content
-def parse_xml(filepath):
-	print filepath
-	content = sqlContext.read.format("com.databricks.spark.xml").option("rowTag", "body.content").load(filepath)
 	try:
-		result = content.take(1)[0][0][-1][1]
-        # no full text !!!
-	except:
-		result = " "
-        return result
+		return x[0][-1][1][0]
+        except:
+		return ""
 
 def process_corpus(x,N):
         # remove punctuations and numbers
 	text = re.sub("[^a-zA-Z]", " ", x)
         # tokenize corpus
-        word_list = word_tokenize(text)
+        # word_list = word_tokenize(text)
+        filtered_words = text.split(" ")
         # filter out stop words
-        filtered_words = [w for w in word_list if w.lower() not in stopwords.words('english')]
+        #filtered_words = [w for w in word_list if w.lower() not in stopwords.words('english')]
         # distinguish cap words
-        filtered_words = [w if w.islower() else 'zzz_' + w.lower() for w in word_list]
-        result = filtered_words
+        #filtered_words = [w if w.islower() else 'zzz_' + w.lower() for w in word_list]
+        result = copy.copy(filtered_words)
         # create n-grams
-        n_list = sc.parallelize(range(2, N+1), N-1)
-        n_grams = n_list.map(lambda x: ['zzz'+'_'.join(list(ngr)) for ngr in ngrams(filtered_words, x)]).flatMap(lambda x: x).collect()	
-        for ng in n_grams:
-		result.append(ng)
+	for x in range(2, N+1):
+		result.extend(['zzz' + str('_'.join(ngr)) for ngr in ngrams(filtered_words, x)])
+
 	return result
+	
+def get_corpus(filepath):
+        # return a rdd of all text corpus
+        textfiles = sqlContext.read.format("com.databricks.spark.xml").option("rowTag", "body.content").load(filepath + "*.xml")
+        return textfiles.map(lambda x: parse_xml(x)).map(lambda x: process_corpus(x,5))
 
 def create_vocabulary(path):
-	files = get_filenames(path)
-	vcb = []
-        #sc.broadcast(vcb)
-	for f in files:
-		#print files
-		#print "\n"
-	#par = sc.parallelize(range(0, len(files)))
-	#corpus = par.map(lambda x: parse_xml(files[x])).collect()	
-		corpus = parse_xml(f)
-		corpus = np.unique(process_corpus(corpus,5))
-	        #all_corpus.append(corpus)
-		vcb.append(corpus)
-	return sorted(np.unique(vcb))
-
+	vcb = get_corpus(path).flatMap(lambda x: x).distinct().collect()
+	return sorted(vcb)
+ 	
 def bag_of_ngrams(text):
 	result = text
 	return result
 	
-vocabulary = create_vocabulary('lda/temp/')
-print(vocabulary)
+def main():
+	vocabulary = create_vocabulary('lda/temp/')
+	print vocabulary
 
-#text = process_corpus((" ").join(content.take(1)[0][0][-1][1]),5)
-#print text
-#print len(text)
+if __name__ == "__main__":
+	main()
+
 '''
 # Load and parse the data
 data = sc.textFile("lda/nips.txt")
